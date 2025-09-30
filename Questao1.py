c90046c0f53f9544022d5c32217ccfeeba3f4c06
arthurlimazx <arthur.lima99@aluno.ce.gov.br>
2025-09-30 10:19:26 -0300

    Atualização

--- Questao1.py
+++ Questao1.py
@@ -1,12 +1,14 @@
 # Convidados começa 
-
-
+contnao=0
+cont=0
 lista=[]
 for v in range(1,6):
-    nome=str(input("Me de o {v} nome que você quer cadastrar: "))
+    nome=str(input(f"Me de o {v} nome que você quer cadastrar: "))
     lista.append(nome)
 print(lista)
 for nome in lista:
     if nome[0]=="a" or nome[0]=="A":
-        lista.remove(nome)
-print(f"As palavra que não começam com a letra A são {lista}")
+        cont+=1
+    else:
+        contnao+=1
+print(f"As palavra que não começam com a letra A são {contnao}")
