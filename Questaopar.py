0c7d7d7264df6d1a5308dfbc1a09b8150c0c5365
arthurlimazx <arthur.lima99@aluno.ce.gov.br>
2025-09-30 10:39:19 -0300

    Atualização

--- Questaopar.py
+++ Questaopar.py
@@ -7,10 +7,11 @@ for x in range(1,11):
     numero=int(input(f"Me de o {x} número: "))
     lista.append(numero)
 for numero in lista:
-    if x%2==0:
+    if numero%2==0:
         numeropar+=numero
-    else:
+    if numero%2!=0:
         numeroimpar+=numero
 
 print(f"a soma do total de números pares foi {numeropar}")    
 print(f"a soma do total de números impares foi {numeroimpar}")    
+
